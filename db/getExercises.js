13cdd3a36ce8895c9aeba69a3c819cda40f3fee6
Sean Urbassik <sean.urbassik@gmail.COM>
2019-06-07 10:24:15 -0400

    still working on json

--- db/getExercises.js
+++ db/getExercises.js
@@ -3,34 +3,36 @@ const fs = require("fs");
 const fetch = require("node-fetch");
 
 let currentPage = 1;
-let exerciseURL = `https://wger.de/api/v2/exercise/?page=
-${currentPage}`;
-// /api/v2/exercise.json
+// let exerciseURL = `https://wger.de/api/v2/exercise/?page=${currentPage}`;
 
 let exercises = [];
 
-for (i = 1; i <= 29; i++) {
-  fetch(exerciseURL + currentPage)
-    .then(res => res.json())
-    .then(idk => exercises.push(idk))
-    .then(() => console.log(exercises))
-    .catch(err => {
-      console.log("something went wrong...", err);
-    });
-}
 /** in the responses, there is a `"next"` key that we're using until we get null */
 
 /** step 1: use fetch to get the exercise data from the wger API, and concatenate the results to `exercieses` until we run out of new pages */
 
 /** step 2: once we hit the end and run out of pages, we will THEN us the `fs` module to write the `exercises` array to a json file */
 
-// fetch(exerciseURL)
-//   .then(res => res.json())
-//   .then(var2 => {
-//     let stringified = JSON.stringify(var2, null, 2);
-//     fs.writeFile("./exercise.json", stringified, "utf8", err => {
-//       if (err) {
-//         console.log(err);
-//       } else console.log(`wrote ${var2.length} items to exercise.json`);
-//     });
-//   });
+function fetchExerciseData() {
+  let pagesRequired = 0;
+
+  fetch(`https://wger.de/api/v2/exercise/?page=1`).then(res => {
+    const exerciesespromise = [];
+    pagesRequired = res.data.pagesRequired;
+
+    for (let i = pagesRequired; i < 0; i++) {
+      exerciesespromise.push(
+        fetch(`https://wger.de/api/v2/exercise/?page=${currentPage}` + i)
+      );
+    }
+
+    Promise.all(exerciesespromise).then(res => {
+      const processedExercises = [];
+      res
+        .map(res => {
+          processedExercises.push(res);
+        })
+        .then(console.log(exerciesespromise));
+    });
+  });
+}
