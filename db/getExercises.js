d3b4af8d989a9c9418bbce90b7aa49e02dc3f18f
Sean Urbassik <sean.urbassik@gmail.COM>
2019-06-10 11:17:52 -0400

    tuned further

--- db/getExercises.js
+++ db/getExercises.js
@@ -2,11 +2,6 @@ const axios = require("axios");
 const fs = require("fs");
 const fetch = require("node-fetch");
 
-/** in the responses, there is a `"next"` key that we're using until we get null */
-
-/** step 1: use fetch to get the exercise data from the wger API, and concatenate the results to `exercieses` until we run out of new pages */
-
-/** step 2: once we hit the end and run out of pages, we will THEN us the `fs` module to write the `exercises` array to a json file */
 let Exercises = [];
 for (i = 0; i <= 29; i++) {
   let exerciseURL = `https://wger.de/api/v2/exercise/?page=${i}`;
